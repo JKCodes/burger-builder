cd06e8910b1edabcd033a9c9b590c15b53f90231
Joe Kim <josephkim0224@gmail.com>
2018-01-25 13:48:31 -0500

    add contact info form

---
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import Button from '../../../components/UI/Button/Button';
+
+import styles from './ContactData.css';
+
+class ContactData extends Component {
+  state = {
+    name: '',
+    email: '',
+    address: {
+      street: '',
+      postalCode: ''
+    }
+  }
+
+  render() {
+    return(
+      <div className={styles.ContactData}>
+        <h4>Enter your Contact Information</h4>
+        <form>
+          <input className={styles.Input} type="text" name="name" placeholder="Your name" />
+          <input className={styles.Input} type="email" name="email" placeholder="Your email" />
+          <input className={styles.Input} type="text" name="street" placeholder="Street" />
+          <input className={styles.Input} type="text" name="postalCode" placeholder="Postal Code" />
+          <Button btnType="Success">ORDER</Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ContactData;
