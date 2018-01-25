d0165e2bfb2b93e81b54c6d9653fd0c091ca2772
Joe Kim <josephkim0224@gmail.com>
2018-01-25 15:26:37 -0500

    add styling for invalid forms

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -4,18 +4,23 @@ import styles from './Input.css'
 
 const input = (props) => {
   let inputElement = null;
+  const inputClasses = [styles.InputElement]
+
+  if (props.invalid && props.shouldValidate) {
+    inputClasses.push(styles.Invalid);
+  }
 
   switch (props.elementType) {
     case ('input'):
       inputElement = <input 
-        className={styles.InputElement} 
+        className={inputClasses.join(' ')} 
         onChange={props.changed}
         {...props.elementConfig} 
         value={props.vaue} />;
       break;
     case ('textarea'):
       inputElement = <textarea 
-        className={styles.InputElement} 
+        className={inputClasses.join(' ')} 
         onChange={props.changed}
         {...props.elementConfig} 
         value={props.vaue} />;
@@ -23,7 +28,7 @@ const input = (props) => {
     case ('select'):
       inputElement = (
         <select 
-          className={styles.InputElement} 
+          className={inputClasses.join(' ')} 
           onChange={props.changed}
           value={props.value}>
           {props.elementConfig.options.map(option => (
@@ -36,7 +41,7 @@ const input = (props) => {
         break;
     default:
       inputElement = <input 
-        className={styles.InputElement} 
+        className={inputClasses.join(' ')} 
         {...props.elementConfig} 
         value={props.vaue} />;
   }
